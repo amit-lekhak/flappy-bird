8ec94851c507f6112190dbb9ce1d3692e96857bc
Amit Lekhak <amitlekhak1@gmail.com>
2023-10-07 20:21:44 +0545

    Add score & retry method

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ const velocityX = -2;
 let velocityY = 0;
 const gravity = 0.4;
 
+let score = 0;
+let gameOver = false;
+
 window.onload = function () {
   board = document.getElementById("board");
 
@@ -64,6 +67,7 @@ window.onload = function () {
 
 const update = () => {
   requestAnimationFrame(update);
+  if (gameOver) return;
 
   // clear canvas on each frame
   context.clearRect(0, 0, boardWidth, boardHeight);
@@ -75,27 +79,58 @@ const update = () => {
   // redraw bird on canvas
   context.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height);
 
+  if (bird.y > boardHeight) {
+    gameOver = true;
+  }
+
   // draw pipes
   for (let i = 0; i < pipeArray.length; i++) {
     let pipe = pipeArray[i];
 
     pipe.x += velocityX;
     context.drawImage(pipe.image, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+      score += 0.5;
+      pipe.passed = true;
+    }
+
+    if (detectCollision(bird, pipe)) {
+      gameOver = true;
+    }
   }
 
   // clear pipes
   if (pipeArray[0] && pipeArray[0].x < -pipeWidth) {
     pipeArray.shift();
   }
+
+  // add score
+  context.fillStyle = "white";
+  context.font = "45px sans-serif";
+  context.fillText(score, 5, 45);
+
+  if (gameOver) {
+    context.fillText("Game Over", 5, 90);
+  }
 };
 
 const moveBird = (event) => {
   if (event.code == "Space" || event.code == "ArrowDown") {
     velocityY = -6;
+
+    if (gameOver) {
+      pipeArray = [];
+      bird.y = birdY;
+      score = 0;
+      gameOver = false;
+    }
   }
 };
 
 const placePipes = () => {
+  if (gameOver) return;
+
   const randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
   const openingSpace = board.height / 4;
 
@@ -121,3 +156,12 @@ const placePipes = () => {
 
   pipeArray.push(bottomPipe);
 };
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
+    a.x + a.width > b.x && //a's top right corner passes b's top left corner
+    a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
+    a.y + a.height > b.y
+  ); //a's bottom left corner passes b's top left corner
+}
